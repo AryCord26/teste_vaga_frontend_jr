684e9766dee171357ff54aee162da2ddd0d69ad3
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-08-14 20:35:25 -0300

    Update Product.ts

--- public/src/types/Product.ts
+++ public/src/types/Product.ts
@@ -1,1 +1,11 @@
+export type Product = {
+  id: string | number;
+  productName: string;
+  descriptionShort?: string;
+  photo: string;
+  price: number;
+  oldPrice?: number;
+  installment?: { count: number; value: number };
+  category?: string;
+};
 
