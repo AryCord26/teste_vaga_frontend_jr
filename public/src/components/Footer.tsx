2980e036cd39038efd7030d47e56e6004ee7fba1
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-08-14 20:39:32 -0300

    Update Footer.tsx

--- public/src/components/Footer.tsx
+++ public/src/components/Footer.tsx
@@ -1,1 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer style={{ padding: '20px', background: '#333', color: '#fff', textAlign: 'center', marginTop: '40px' }}>
+      © 2025 Econverse. Todos os direitos reservados.
+    </footer>
+  );
+}
 
