0143176a35dfbaa27da60105f2d8aa5a03ef4919
Ary Cordeiro <ary_cordeiro@outlook.com>
2025-08-14 20:39:24 -0300

    Update Header.tsx

--- public/src/components/Header.tsx
+++ public/src/components/Header.tsx
@@ -1,1 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header style={{ padding: '20px', background: '#FF2E6A', color: '#fff', textAlign: 'center', fontWeight: 'bold', fontSize: '1.5rem' }}>
+      Econverse Vitrine
+    </header>
+  );
+}
 
